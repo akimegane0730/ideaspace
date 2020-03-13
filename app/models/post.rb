f9d6180d6e891359acb6aafd898211ac9d2ce829
akimegane0730 <akimegane0730o@gmail.com>
2020-03-13 15:47:36 +0900

    set model association

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   accepts_nested_attributes_for :comments, allow_destroy: true
 
